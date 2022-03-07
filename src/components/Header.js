fdb0d2ddd90f0a630b3fde172914314dd452b40e
Alp Tarla <alptarla@gmail.com>
2022-03-07 11:28:25 +0300

    Add header to dashboard

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Header.module.css'
+
+function Header() {
+  return (
+    <header className={classes.header}>
+      <div className="container">
+        <h1>X Documents</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Header from '../components/Header'
 
 function Dashboard() {
-  return <div>Dashboard</div>
+  return (
+    <div>
+      <Header />
+      <main className="container">Dashboard</main>
+    </div>
+  )
 }
 
 export default Dashboard
