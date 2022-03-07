f6fc2ac96f64a7853106b2c386681a6026f4327d
Alp Tarla <alptarla@gmail.com>
2022-03-07 16:21:13 +0300

    Refactor component structure

--- src/components/DocumentList/DocumentList.js
+++ src/components/DocumentList/DocumentList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import DocumentCard from './DocumentCard'
+import DocumentCard from '../DocumentCard/DocumentCard'
 import classes from './DocumentList.module.css'
 
 function DocumentList({ documents }) {
