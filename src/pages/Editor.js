efeaeef57f0616c15f32d0015479bbce77bad699
Alp Tarla <alptarla@gmail.com>
2022-03-07 14:16:00 +0300

    Add text editor with Quill Text Editor lib to editor page

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
+import React, { useCallback, useState } from 'react'
+import { TOOLBAR_OPTIONS } from '../config/editorConfig'
 
 function Editor() {
-  const { id: documentId } = useParams()
+  const [quill, setQuill] = useState(null)
 
-  return <div>Editor {documentId}</div>
+  const editorWrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return
+
+    wrapper.innerHTML = ''
+    const editorEl = document.createElement('div')
+    wrapper.append(editorEl)
+
+    const editor = new Quill(editorEl, {
+      theme: 'snow',
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    })
+
+    editor.disable()
+    editor.setText('Loading...')
+
+    setQuill(editor)
+  }, [])
+
+  return (
+    <div>
+      <main className="editor" ref={editorWrapperRef}></main>
+    </div>
+  )
 }
 
 export default Editor
